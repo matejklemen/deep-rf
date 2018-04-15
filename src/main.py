77cb63237be56902a830655c8b0c88cb71fa74a1
Matej Klemen <matej.klemen1337@gmail.com>
2018-04-15 23:08:32 +0200

    Change example

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from csv import reader
 import numpy as np
+import pandas as pd
 import decision_tree
 
 TRAINING_SIZE = 0.8
@@ -8,11 +9,15 @@ if __name__ == "__main__":
     np.random.seed(1337)
 
     with open("../data/iris_data.csv") as iris_dataset:
-        reader_obj = reader(iris_dataset, delimiter=",")
+        df = pd.read_csv(iris_dataset, delimiter=",", header=None)
+        df = df.sample(frac=1).reset_index(drop=True)
 
-        dataset = np.array(list(reader_obj))
-        np.random.shuffle(dataset)
+        training_set_X = np.array(df.iloc[0: int(TRAINING_SIZE * len(df)), 0: 4])
+        training_set_y = np.array(df.iloc[0: int(TRAINING_SIZE * len(df)), 4])
 
-        # Note to self: currently all strings -> split input features and labels
-        training_set, test_set = dataset[:int(TRAINING_SIZE * np.size(dataset, axis=0)), :], \
-                                 dataset[int(TRAINING_SIZE * np.size(dataset, axis=0)):, :]
+        test_set_X = np.array(df.iloc[int(TRAINING_SIZE * len(df)):, 0: 4])
+        test_set_y = np.array(df.iloc[int(TRAINING_SIZE * len(df)):, 4])
+
+        dt = decision_tree.DecisionTree(max_depth=3)
+        dt.fit(training_set_X, training_set_y)
+        dt.traverse()
