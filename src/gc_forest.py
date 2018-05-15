11ddfcefab0acbd77dce14ca1bac90b49318cdf9
Matej Klemen <matej.klemen1337@gmail.com>
2018-05-15 22:48:19 +0200

    First working version of cascade forest structure

--- src/gc_forest.py
+++ src/gc_forest.py
@@ -7,16 +7,17 @@ def train_test_split(features, labels, test_size):
     :param test_size: float between 0 and 1
     :return: (features_train, labels_train, features_test, labels_test)
     """
-    return (features[:int(test_size * features.shape[0]), :], labels[:int(test_size * features.shape[0])],
-            features[int(test_size * features.shape[0]):, :], labels[int(test_size) * features.shape[0]:])
+    return (features[:int((1 - test_size) * features.shape[0]), :], labels[:int((1 - test_size) * features.shape[0])],
+            features[int((1 - test_size) * features.shape[0]):, :], labels[int((1 - test_size) * features.shape[0]):])
 
 
 class GCForest:
     # TODO: add k_cv (for k-fold cv), add val_size (for evaluating cascade after adding new level),
     # max_cascade_layers
-    def __init__(self, nforests_cascade=4, label_idx_mapping=None):
+    def __init__(self, nrforests_layer=4, ncrforests_layer=4, label_idx_mapping=None):
 
-        self.nforests_cascade = nforests_cascade
+        self.nrforests_layer = nrforests_layer
+        self.ncrforests_layer = ncrforests_layer
 
         self.label_idx_mapping = label_idx_mapping
         self.idx_label_mapping = None
@@ -47,51 +48,79 @@ class GCForest:
         idx_curr_layer = 0
         self._cascade = []
         curr_input = X_train
+        # TODO: double check whether decision to add new layer is based on previous layer accuracy or overall accuracy
+        best_acc = 0
 
         while True:
-            if idx_curr_layer >= self.max_cascade_depth:
-                print("Achieved max allowed depth for gcForest. Exiting...")
+            if self._num_layers >= self.max_cascade_depth:
+                print("[fit()] Achieved max allowed depth for gcForest. Exiting...")
                 break
 
-            print("Training layer %d..." % idx_curr_layer)
-            curr_layer_models, new_features = self._cascade_layer()
+            print("[fit()] Training layer %d..." % self._num_layers)
+            curr_layer_models, new_features = self._cascade_layer(curr_input, y_train)
 
-            print("Shape of new features: ")
-            print(new_features.shape)
-            # TODO: check performance of cascade on validation set
-            # ...
-
-            # if performance keeps on improving, store current layer
+            # expand new level
             self._cascade.append(curr_layer_models)
-            idx_curr_layer += 1
-            self._num_layers = idx_curr_layer
-            # else
-            # stop building new layers, average class probabilities in new_features and select class with max probability
+            self._num_layers += 1
+
+            print("[fit()] Shape of new features: ")
+            print(new_features.shape)
+            # check performance of cascade on validation set
+            new_layer_acc = self._eval_cascade(X_val, y_val)
+            print("[fit()] New layer accuracy is %.3f..." % new_layer_acc)
+
+            if self._num_layers == 0:
+                print("[fit()] Setting first best accuracy to %.3f..." % new_layer_acc)
+                best_acc = new_layer_acc
+
+            # drop in accuracy on validation set is bigger than allowed
+            if new_layer_acc <= best_acc:
+                print("[fit()] New layer accuracy (%.3f) is <= than overall best accuracy (%.3f),"
+                      " therefore no more layers will be added to cascade..."
+                      % (best_acc, new_layer_acc))
+                break
+            else:
+                print("[fit()] Setting new best accuracy to %.3f..." % new_layer_acc)
+                best_acc = new_layer_acc
 
             curr_input = np.hstack((X_train, new_features))
-            self._num_layers = idx_curr_layer
 
-    # TODO: test this (function is untested
-    def predict(self, X_test, predict_probabilities=False):
+        print("[fit()] Final verdict: num_layers = %d, best accuracy obtained: %3f..." % (self._num_layers, best_acc))
+
+    def predict(self, X_test):
+        return self._predict(X_test, predict_probabilities=False)
+
+    def predict_proba(self, X_test):
+        return self._predict(X_test, predict_probabilities=True)
+
+    def _mg_scan(self):
+        pass
+
+    def _predict(self, X_test, predict_probabilities=False):
+        if self._num_layers <= 0:
+            raise Exception("[predict()] Number of layers is <= 0...")
+
         num_labels = len(self.label_idx_mapping)
 
         curr_input = np.array(X_test)
 
         for idx_layer in range(self._num_layers):
+            print("[predict()] Going through layer %d..." % idx_layer)
             curr_layer_models = self._cascade[idx_layer]
 
-            new_features = np.zeros(curr_input.shape[0], self.nforests_cascade * num_labels)
+            new_features = np.zeros((curr_input.shape[0], (self.ncrforests_layer + self.nrforests_layer) * num_labels))
             for idx_model in range(len(curr_layer_models)):
                 new_features[:, idx_model * num_labels: (idx_model + 1) * num_labels] += \
-                    curr_layer_models[idx_model].predict(curr_input, return_probabilities=True)
+                    curr_layer_models[idx_model].predict_proba(curr_input)
 
             # last layer: get class distributions (normal procedure) and average them to obtain final distribution
             if idx_layer == self._num_layers - 1:
+                print("[predict()] Got to the last level...")
                 final_probs = np.zeros((curr_input.shape[0], num_labels))
-                for idx_model in range(self.nforests_cascade):
+                for idx_model in range(len(curr_layer_models)):
                     final_probs += new_features[:, idx_model * num_labels: (idx_model + 1) * num_labels]
 
-                final_probs = np.divide(final_probs, self.nforests_cascade)
+                final_probs = np.divide(final_probs, len(curr_layer_models))
 
                 if predict_probabilities:
                     return final_probs
@@ -104,34 +133,61 @@ class GCForest:
 
             # all but the last layer: get the input concatenated with obtained class distribution vectors
             else:
+                print("[predict()] I ain't fucking leaving! Concatenating input with new features...")
                 curr_input = np.hstack((X_test, new_features))
 
-    def _eval_cascade(self):
-        pass
+    def _eval_cascade(self, X_val, y_val):
+        """ Evaluates currently built cascade
+        :param X_val: validation set features
+        :param y_val: validation set labels
+        :return: accuracy of cascade
+        """
+        preds = self._predict(X_val)
+
+        cascade_acc = np.sum(preds == y_val) / y_val.shape[0]
+        print("[_eval_cascade()] Evaluated cascade and got accuracy %.3f..." % cascade_acc)
+
+        return cascade_acc
 
     def _cascade_layer(self, X, y):
         """
         :param X: input data (features)
         :param y: labels
-        :return:
+        :return: (list of trained models for current layer, distribution vector for current layer)
         """
 
         num_labels = len(self.label_idx_mapping)
         curr_layer_models = []
-        curr_layer_distributions = np.zeros(X.shape[0], self.nforests_cascade * num_labels)
+        curr_layer_distributions = np.zeros((X.shape[0], (self.ncrforests_layer + self.nrforests_layer) * num_labels))
 
-        for idx_curr_forest in range(self.nforests_cascade):
-            # TODO add completely random forests
+        # -- completely random forests --
+        for idx_curr_forest in range(self.ncrforests_layer):
+            print("Training completely random forest number %d..." % idx_curr_forest)
             # each random forest produces a (#classes)-dimensional vector of class distribution
-            curr_rf, curr_class_distrib = self._get_class_distrib(X, y)
+            rf_obj = random_forest.RandomForest(label_idx_mapping=self.label_idx_mapping,
+                                                n_estimators=500,
+                                                extremely_randomized=True)
+            curr_rf, curr_class_distrib = self._get_class_distrib(X, y, rf_obj)
 
             curr_layer_models.append(curr_rf)
             curr_layer_distributions[:, idx_curr_forest * num_labels: (idx_curr_forest + 1) * num_labels] += \
                 curr_class_distrib
 
-        return (curr_layer_models, curr_layer_distributions)
+        # -- random forests --
+        for idx_curr_forest in range(self.nrforests_layer):
+            print("Training random forest number %d..." % idx_curr_forest)
+            # each random forest produces a (#classes)-dimensional vector of class distribution
+            rf_obj = random_forest.RandomForest(label_idx_mapping=self.label_idx_mapping, n_estimators=500)
+            curr_rf, curr_class_distrib = self._get_class_distrib(X, y, rf_obj)
 
-    def _get_class_distrib(self, X_train, y_train):
+            curr_layer_models.append(curr_rf)
+            curr_layer_distributions[:, (self.ncrforests_layer + idx_curr_forest) * num_labels:
+                                        (self.ncrforests_layer + idx_curr_forest + 1) * num_labels] += \
+                curr_class_distrib
+
+        return curr_layer_models, curr_layer_distributions
+
+    def _get_class_distrib(self, X_train, y_train, model):
         """ Obtains class distribution of a single random forest in a cascade layer.
         :param X_train: training data (features)
         :param y_train: training data (labels)
@@ -148,16 +204,14 @@ class GCForest:
             curr_train_X, curr_train_y = X_train[np.logical_not(curr_test_mask), :], y_train[np.logical_not(curr_test_mask)]
             curr_test_X, curr_test_y = X_train[curr_test_mask, :], y_train[curr_test_mask]
 
-            rf = random_forest.RandomForest(label_idx_mapping=self.label_idx_mapping)
-            rf.fit(curr_train_X, curr_train_y)
-
-            class_distrib[curr_test_mask, :] += rf.predict(curr_test_X, return_probabilities=True)
+            model.fit(curr_train_X, curr_train_y)
+            # careful about vector index - label relationship (certain index should represent same label in all cases)
+            class_distrib[curr_test_mask, :] += model.predict_proba(curr_test_X)
 
         # train a RF model on whole training set, will be placed in cascade
-        rf = random_forest.RandomForest(label_idx_mapping=self.label_idx_mapping)
-        rf.fit(X_train, y_train)
+        model.fit(X_train, y_train)
 
-        return (rf, class_distrib)
+        return model, class_distrib
 
     def _kfold_cv(self, num_examples, k):
         """ Prepare groups for k-fold cross validation.
@@ -169,7 +223,3 @@ class GCForest:
         bins = np.digitize(np.arange(0, num_examples), limits) - 1
 
         return np.random.permutation(bins)
-
-    def predict(self):
-        pass
-
